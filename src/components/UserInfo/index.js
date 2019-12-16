ef7ea55e04996bdc8cb98d17aa3c1caf0ea86232
Gasty-WebDev113 <gastymorales113@gmail.com>
2019-12-16 17:54:27 -0300

    Add Activity and Blogs UI in the account

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,27 +1,35 @@
-import React from 'react'
-import {InfoContainer, UsernameTitle, UserPhoto, UserPhotoAlternative} from './styles'
+import React, {useState} from 'react'
+import {InfoContainer, UsernameTitle, UserPhoto, UserPhotoAlternative, UserInfoContainer, Changer, Activity, ButtonMode, Blogs} from './styles'
 import { useQuery } from '@apollo/react-hooks';
 import {GET_USER} from '../../constants/gqltags'
 import {Loader} from '../Loader'
 
 export const UserInfo = ({username}) => {
 
+    const [mode, setmode] = useState(false)
+
     const {loading, error, data} = useQuery(GET_USER,{
         variables: {Username: username}
     })
-    
-    console.log(loading)
 
     if(loading === false){
     const { Username, Email, FullName } = data.getUser
     return(
         <InfoContainer>
-            <UserPhoto>
-                <UserPhotoAlternative>{username.slice(0,1).toUpperCase()}</UserPhotoAlternative>
-            </UserPhoto>
-            <UsernameTitle>{Username}</UsernameTitle>
-            <UsernameTitle>{Email}</UsernameTitle>
-            <UsernameTitle>{FullName}</UsernameTitle>    
+            <UserInfoContainer>
+                <UserPhoto>
+                    <UserPhotoAlternative>{username.slice(0,1).toUpperCase()}</UserPhotoAlternative>
+                </UserPhoto>
+                <UsernameTitle>{Username}</UsernameTitle>
+                <UsernameTitle>{Email}</UsernameTitle>
+                <UsernameTitle>{FullName}</UsernameTitle> 
+            </UserInfoContainer>   
+
+            <Changer>
+                <ButtonMode movement={mode}></ButtonMode>
+                <Activity lettercolors={mode} onClick={() => setmode(true)}>Activity</Activity>
+                <Blogs lettercolors={!mode} onClick={() => setmode(false)}>Blogs</Blogs>
+            </Changer>
         </InfoContainer>
     )}else{
         return <Loader />
--- src/components/UserInfo/styles.js
+++ src/components/UserInfo/styles.js
@@ -10,6 +10,20 @@ export const InfoContainer = styled.div`
         padding-top: 20%;
     }
 `
+export const UserInfoContainer = styled.div`
+  position: absolute;
+  min-width: 40vw;
+  background: -webkit-linear-gradient(top, rgba(158,158,158,1) 0%, rgba(245,245,245,1) 100%);
+  min-height: 70%;
+  margin-left: 5vw;
+  @media (min-width: 1090px){
+    min-width: 30%;
+    }
+  @media (max-width: 768px){
+    margin-top: 5vh;
+    }
+`
+
 export const UserPhoto = styled.div`
   background: gray;
   height: 15vw;
@@ -18,8 +32,8 @@ export const UserPhoto = styled.div`
   cursor: pointer;
   align-items: center;
   margin: 0 auto;
-  margin-bottom: 5vw;
-  margin-top: 5%;
+  margin-bottom: 5vh;
+
   @media (min-width: 770px){
     margin-top: 0;
     }
@@ -34,10 +48,81 @@ export const UserPhotoAlternative = styled.h1`
   vertical-align: middle;
   color: white !important;
   text-decoration: none;
-
+  margin-top: 5vh;
 `;
 
 export const UsernameTitle = styled.h1`
+  position: relative;
+  text-align: center;
+  font-size: 2vw;
+
+  @media (max-width: 1225px){
+    font-size: 3vw;
+    }
+  @media (max-width: 426px){
+    font-size: 4vw;
+    }
+`
+
+export const Changer = styled.div`
+    width: 25vw;
+    height: 5vh;
+    margin-top: 5vh;
+    margin-bottom: 3vh;
+    right: 15vw;
+    position: absolute;
+    @media (max-width: 768px){
+      width: 25vw;
+    }
+    @media (max-width: 426px){
+      width: 35vw;
+      right: 10vw;
+    }
+    
+`
+export const Activity = styled.div`
     position: relative;
+    height: 5vh;
+    float: left;
     text-align: center;
+    line-height: 5vh;
+    /*Sorry, I could't solve in another shape*/
+    color: ${props => props.lettercolors ? 'white' : 'orange' };
+    cursor: pointer;
+    z-index: 999;
+
+    @media (max-width: 768px){
+        font-weight: 2vw
+    }
+    @media (max-width: 426px){
+        font-weight: 1vw
+    }
+`
+
+export const Blogs = styled(Activity)`
+    float: right;
+`
+
+export const ButtonMode = styled.div`
+    position: absolute;
+    width: 8vw;
+    height: 5vh;
+    background: blue;
+    border-radius: 28px;
+    transform: translateX(${props => props.movement ? '-1.5vw' : '19vw' });
+    transition-duration: 0.25s;
+
+    @media (max-width: 768px){
+      width: 10vw;
+      transform: translateX(${props => props.movement ? '-1.5vw' : '17.5vw' });
+    }
+    @media (max-width: 426px){
+      width: 15vw;
+      transform: translateX(${props => props.movement ? '-1.5vw' : '22.5vw' });
+    }
+    @media (max-width: 376px){
+      width: 18vw;
+      transform: translateX(${props => props.movement ? '-1.5vw' : '20.5vw' });
+    }
+    
 `
